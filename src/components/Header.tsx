5ff7fa937390aa36b142833e1a959c3eec942381
Grace Desrochers <gracedes@bu.edu>
2026-03-03 18:16:28 -0500

    header element styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,27 @@
+import styled from 'styled-components';
+
+import {Window, WinDeco, Content} from "./styling/WindowStyles.tsx";
+
 export default function Header() {
+    const Header = styled.header`
+        width: calc(100% - 2 * 0.5vw);
+        height: 15%;
+        & h1 { font-size: calc(2px + max(4vh, 3vw)) };
+        & p {
+            font-size: calc(2px + max(2.5vh, 1.5vw));
+            font-style: italic;
+        };
+    `;
+
     return (
-        <header>
-            <h1>Grace Desrochers</h1>
-            <p>Welcome to my online resume!</p>
-        </header>
+        <Header>
+            <Window>
+                <WinDeco><img src="../../public/window.png" alt="decorative minimize, expand, and close icons"/></WinDeco>
+                <Content>
+                    <h1>Grace Desrochers</h1>
+                    <p>Welcome to my online resume!</p>
+                </Content>
+            </Window>
+        </Header>
     );
 }
